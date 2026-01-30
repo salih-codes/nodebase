17257b85b1c10654eee7e914f1827bad2b294aec
salih-codes <salih.codes@proton.me>
2026-01-30 10:49:03 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+	return (
+		<div className="min-h-screen min-w-screen flex items-center justify-center">
+			<Button>Click me</Button>
+		</div>
+	);
+}
